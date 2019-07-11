6a7c026d311d095752b171133762881be0c7a33f
geetika.arora <geetika.arora@mobileprogramming.com>
2019-07-11 18:01:25 +0530

    new

--- server.js
+++ server.js
@@ -4,3 +4,6 @@ var app             =   express();
 app.listen(8888);
 console.log('Listening to port 8888');
 console.log('Hell world');
+app.get('/',function(req,res){
+res.send("hello hws u?");
+})
